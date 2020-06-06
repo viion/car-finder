fc1e3762118c14085ea9ae6f0baf06ff3191cf4e
Josh Freeman <josh@viion.co.uk>
2020-06-06 18:54:17 +0100

    Changes

--- website/src/Controller/DefaultController.php
+++ website/src/Controller/DefaultController.php
@@ -40,6 +40,7 @@ class DefaultController extends AbstractController
         
         $filter = [
             'fave' => $fave,
+            'deleted' => false,
         ];
         
         $log = $this->parser->getLog();
